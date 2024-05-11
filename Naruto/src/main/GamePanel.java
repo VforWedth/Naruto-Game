8f3313b47833bf3299c429d423773185c487be5f
Shon Lei Zaw <slei53922@gmail.com>
2024-05-11 12:45:45 +0630

    updating sprite

--- Naruto/src/main/GamePanel.java
+++ Naruto/src/main/GamePanel.java
@@ -17,9 +17,10 @@ public class GamePanel extends JPanel {
 	private MouseInputs mouseInputs;
 	private float xDelta = 100, yDelta = 100; 
 	private BufferedImage img, subImg;
-	private String source;
+	private String source = null;
 	//private float xPos, yPos, Height, Width;
-	private Scanner input = new Scanner(System.in);
+	Scanner input = new Scanner(System.in);
+	private int charX, charY;
 
 	public GamePanel() {
 		
@@ -32,35 +33,50 @@ public class GamePanel extends JPanel {
 		addMouseMotionListener(mouseInputs);
 	}
 	
-	public void chooseCharacter() {
-		System.out.println("Choose the player: \n 1)Naruto\n2)Kakashi\n3)Sakura");
+	public String chooseCharacter() {
+		System.out.println("Choose the player: \n1)Uzumaki Naruto\n2)Hatake Kakashi\n3)Uchiha Sasuke\n4)Sakura Haruno");
 		int num = input.nextInt();
 		switch(num) {
 		case 1:
 			source = "/player.png";
+			charX = 0;
+			charY = 2*133;
 			break;
 		case 2:
-			source = "/KakashiSprite.png";
+			source = "/HatakeKakashi.png";
+			charX = 6*123;
+			charY = 0;
 			break;
 		case 3:
-			source = "/SakuraSprite.png";
+			source = "/UchihaSasuke.png";
+			charX = 6*123;
+			charY = 0;
+			break;
+		case 4:
+			source = "/SakuraHaruno.png";
+			charX = 6*123;
+			charY = 0;
+			break;
+		default:
+			System.out.println("Invalid choice!");
 			break;
 		}
+		return source;
 		
 	}
 	
 	private void importImage() {
-		InputStream is = getClass().getResourceAsStream("source");
+		InputStream is = getClass().getResourceAsStream(source);
 		try {
 			img = ImageIO.read(is);
 		}catch(IOException e) {
 			e.printStackTrace();
 		}
-		
 	}
 	
 	public void setPanelSize() {
 		Dimension size = new Dimension(1000, 600);
+		setPreferredSize(size);
 		setMinimumSize(size);
 		setMaximumSize(size);
 	}
@@ -78,10 +94,10 @@ public class GamePanel extends JPanel {
 		this.yDelta = y;
 	}
 
-	public void painComponent(Graphics g) {
+	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
-		subImg = img.getSubimage(5*123, 0, 125, 133);
+		subImg = img.getSubimage(charX, charY, 125, 133);
 		g.drawImage(subImg, (int)xDelta, (int)yDelta, 125, 133, null);
 	}
 }
