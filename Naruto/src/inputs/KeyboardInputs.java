628c58c54426edce2f051c76670efacf8e994f1c
VforWedth <lorendrain47@gmail.com>
2024-05-13 10:08:28 +0630

    Update KeyboardInputs.java
    Update

--- Naruto/src/inputs/KeyboardInputs.java
+++ Naruto/src/inputs/KeyboardInputs.java
@@ -2,48 +2,60 @@ package inputs;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-
+import static utliz.Constants.Direction.*;
+import static utliz.Constants.PlayerConstants.*;
 import main.GamePanel;
 
-public class KeyboardInputs implements KeyListener {
+public class Keyboardinputs implements KeyListener {
 	
 	private GamePanel gamePanel;
-	
-	public KeyboardInputs(GamePanel gamePanel) {
+	public Keyboardinputs(GamePanel gamePanel) {
 		this.gamePanel = gamePanel;
+		
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-
+		// TODO Auto-generated method stub
+		
 		
 	}
 
-	@Override
-	public void keyPressed(KeyEvent e) {
-		switch (e.getKeyCode()) {
-		
-		case KeyEvent.VK_A:
-			gamePanel.changeXDelta(-5);
-			break;
-		case KeyEvent.VK_S:
-			gamePanel.changeYDelta(5);
-			break;
-		case KeyEvent.VK_D:
-			gamePanel.changeXDelta(5);
-			break;
-		case KeyEvent.VK_W:
-			gamePanel.changeYDelta(-5);
-			break;
-		}
-		
-	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+	    switch(e.getKeyCode()) {
+	        case KeyEvent.VK_W:
+	        case KeyEvent.VK_A:
+	        case KeyEvent.VK_S:
+	        case KeyEvent.VK_D:
+	            gamePanel.setMoving(false);
+	            break;
+	    }
 	}
 
 	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		
+		switch(e.getKeyCode()) {
+		case KeyEvent.VK_W:
+			gamePanel.setDirection(UP);
+			break;
+			
+		case KeyEvent.VK_A:
+			gamePanel.setDirection(LEFT);
+			break;
+		
+		case KeyEvent.VK_S:
+			gamePanel.setDirection(DOWN);
+			break;
+			
+		case KeyEvent.VK_D:
+			gamePanel.setDirection(RIGHT);
+			break;
+		}
+	}
+
+
 }
